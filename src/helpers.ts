72c63a57f320a878382d66165fa359fb4530a972
CRIMX <straybugs@gmail.com>
2019-07-20 14:22:13 +0800

    feat: add helpers

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,8 +1,79 @@
+/**
+ * Useful utilities
+ */
 import { useRef, MutableRefObject } from 'react'
-import { Subject } from 'rxjs'
+import { Subject, Observable } from 'rxjs'
+import { pluck } from 'rxjs/operators'
+
+/**
+ * Returns the first argument it receives.
+ */
+export function identity<T>(value: T): T {
+  return value
+}
+
+/**
+ * Transform an Observable of Arraylike to an Observable
+ * of the first item.
+ *
+ * Example:
+ *
+ * ```typescript
+ * const text$ = useObservable(pluckFirst, [props.text])
+ * ```
+ *
+ */
+export function pluckFirst<T extends ArrayLike<any>>(
+  inputs$: Observable<T>
+): Observable<T[0]> {
+  return pluck<T, 0>(0)(inputs$)
+}
+
+/**
+ * Transform an Observable of DOM events to an Observable
+ * of the currentTarget value.
+ *
+ * Example:
+ *
+ * ```typescript
+ * const [onChange, textChange$] = useObservableCallback<
+ *  string,
+ *  React.FormEvent<HTMLInputElement>
+ * >(pluckCurrentTargetValue)
+ * ```
+ *
+ */
+export function pluckCurrentTargetValue<
+  T extends React.SyntheticEvent<any> = React.SyntheticEvent<Element>
+>(events$: Observable<T>): Observable<T['currentTarget']['value']> {
+  return pluck<T, 'currentTarget', 'value'>('currentTarget', 'value')(events$)
+}
+
+/**
+ * Transform an Observable of DOM events to an Observable
+ * of the currentTarget checked.
+ *
+ * Example:
+ *
+ * ```typescript
+ * const [onChange, textChange$] = useObservableCallback<
+ *  string,
+ *  React.FormEvent<HTMLInputElement>
+ * >(pluckCurrentTargetChecked)
+ * ```
+ *
+ */
+export function pluckCurrentTargetChecked<
+  T extends React.SyntheticEvent<any> = React.SyntheticEvent<Element>
+>(events$: Observable<T>): Observable<T['currentTarget']['checked']> {
+  return pluck<T, 'currentTarget', 'checked'>('currentTarget', 'checked')(
+    events$
+  )
+}
 
 /**
  * Return a empty Subject
+ * @ignore
  */
 export function getEmptySubject<T>() {
   return new Subject<T>()
@@ -10,6 +81,7 @@ export function getEmptySubject<T>() {
 
 /**
  * One-time ref init
+ * @ignore
  */
 export function useRefFn<T extends NonNullable<object>>(init: () => T) {
   const ref = useRef<T | null>(null)
@@ -21,6 +93,7 @@ export function useRefFn<T extends NonNullable<object>>(init: () => T) {
 
 /**
  * Is next micro task tick
+ * @ignore
  */
 export function useAsync() {
   const isAsyncRef = useRef(false)
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,9 @@ export { useObservableCallback } from './use-observable-callback'
 export { useSubscription } from './use-subscription'
 export { useObservableState } from './use-observable-state'
 export { useObservablePropsCallback } from './use-observable-props-callback'
+export {
+  identity,
+  pluckFirst,
+  pluckCurrentTargetValue,
+  pluckCurrentTargetChecked
+} from './helpers'
