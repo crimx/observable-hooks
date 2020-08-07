0786ee7a2402c91d20bc956475b99d6066451519
crimx <straybugs@gmail.com>
2020-08-08 01:19:41 +0800

    test: add tests for useObservableEagerState

--- packages/observable-hooks/__tests__/concurrent-mode.spec.tsx
+++ packages/observable-hooks/__tests__/concurrent-mode.spec.tsx
@@ -235,6 +235,112 @@ describe('Concurrent Mode', () => {
     })
   })
 
+  describe('useObservableEagerState', () => {
+    it('should not tear if a mutation occurs during a concurrent update', () => {
+      jest.useFakeTimers()
+
+      const input$ = new Rx.BehaviorSubject('A')
+
+      const Subscriber = ({ id }: { id: string }) => {
+        const value = ObservableHooks.useObservableEagerState(input$)
+        Scheduler.unstable_yieldValue(`render:${id}:${value}`)
+        return (value as unknown) as React.ReactElement
+      }
+
+      act(() => {
+        ReactTestRenderer.create(
+          <React.Fragment>
+            <Subscriber id="first" />
+            <Subscriber id="second" />
+          </React.Fragment>,
+          { unstable_isConcurrent: true } as any
+        )
+        expect(Scheduler).toFlushAndYield(['render:first:A', 'render:second:A'])
+
+        // Update state "A" -> "B"
+        // This update will be eagerly evaluated,
+        // so the tearing case this test is guarding against would not happen.
+        input$.next('B')
+        expect(Scheduler).toFlushAndYield(['render:first:B', 'render:second:B'])
+
+        // No more pending updates
+        jest.runAllTimers()
+
+        // Partial update "B" -> "C"
+        // Interrupt with a second mutation "C" -> "D".
+        // This update will not be eagerly evaluated,
+        // but useObservableState() should eagerly close over the updated value to avoid tearing.
+        input$.next('C')
+        expect(Scheduler).toFlushAndYieldThrough(['render:first:C'])
+        input$.next('D')
+        expect(Scheduler).toFlushAndYield([
+          'render:second:C',
+          'render:first:D',
+          'render:second:D'
+        ])
+
+        // No more pending updates
+        jest.runAllTimers()
+      })
+    })
+
+    it('should ignore errors when the observable is changed but new subscription is not yet established', () => {
+      function Subscription({
+        source$
+      }: {
+        source$: RxType.Observable<string>
+      }) {
+        const state = ObservableHooks.useObservableEagerState(source$)
+        Scheduler.unstable_yieldValue(`render:${state}`)
+        return null
+      }
+
+      const observableA = new Rx.BehaviorSubject('a-0')
+      const observableB = new Rx.BehaviorSubject('b-0')
+
+      let renderer: ReturnType<typeof ReactTestRenderer.create>
+      act(() => {
+        renderer = ReactTestRenderer.create(
+          <Subscription source$={observableA} />,
+          { unstable_isConcurrent: true } as any
+        )
+      })
+
+      expect(Scheduler).toHaveYielded(['render:a-0'])
+
+      act(() => {
+        renderer.update(<Subscription source$={observableB} />)
+
+        // Start React update, but don't finish
+        expect(Scheduler).toFlushAndYieldThrough(['render:a-0'])
+
+        observableA.next('a-1')
+
+        // observableA is ignored
+        expect(Scheduler).toFlushAndYield(['render:b-0'])
+
+        observableA.next('a-2')
+        observableB.next('b-2')
+
+        // observableA is still ignored
+        expect(Scheduler).toFlushAndYield(['render:b-2'])
+      })
+
+      act(() => {
+        renderer.update(<Subscription source$={observableA} />)
+
+        // Start React update, but don't finish
+        expect(Scheduler).toFlushAndYieldThrough(['render:b-2'])
+
+        observableB.next('b-3')
+        observableB.error(new Error('opps'))
+
+        // observableB is ignored
+        expect(Scheduler).toFlushAndYield(['render:a-2'])
+      })
+    })
+  })
+
   describe('ObservableResource', () => {
     it('should not tear if a mutation occurs during a concurrent update', () => {
       jest.useFakeTimers()
---
+++ packages/observable-hooks/__tests__/use-observable-eager-state.spec.ts
@@ -0,0 +1,48 @@
+import { useObservableEagerState } from '../src'
+import { renderHook, act } from '@testing-library/react-hooks'
+import { of, BehaviorSubject, throwError } from 'rxjs'
+import { tap } from 'rxjs/operators'
+
+describe('useObservableEagerState', () => {
+  it('should start receiving values after first rendering', () => {
+    const spy = jest.fn()
+    const outer$ = of(1, 2, 3)
+    const { result } = renderHook(() => {
+      const state = useObservableEagerState(outer$)
+      spy(state)
+      return state
+    })
+    expect(result.current).toBe(3)
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith(3)
+  })
+
+  it('should update value when the Observable emits value', () => {
+    const outer$$ = new BehaviorSubject(1)
+    const { result } = renderHook(() => useObservableEagerState(outer$$))
+    expect(result.current).toBe(1)
+    act(() => {
+      outer$$.next(2)
+    })
+    expect(result.current).toBe(2)
+    act(() => {
+      outer$$.next(3)
+    })
+    expect(result.current).toBe(3)
+  })
+
+  it('should subscribe the twice by dafult', () => {
+    const spy = jest.fn()
+    const outer$$ = of(1).pipe(tap(spy))
+    const { result } = renderHook(() => useObservableEagerState(outer$$))
+    expect(result.current).toBe(1)
+    expect(spy).toHaveBeenCalledTimes(2)
+  })
+
+  it('should throw error when observable emits error', () => {
+    const outer$ = throwError(new Error('opps'))
+    const { result } = renderHook(() => useObservableEagerState(outer$))
+    expect(result.error).toBeInstanceOf(Error)
+    expect(result.error.message).toBe('opps')
+  })
+})
